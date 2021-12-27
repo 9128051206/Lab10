131417686f1c8d80d99d0e9d6d063068bca7110a
91280 <91280@HP-ONQCSD8>
2021-12-27 13:46:22 +0500

    Добавьте файлы проекта.

---
+++ Lab10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] arge)
+        {
+            Corner corner = new Corner();
+            corner.ToRadians();
+
+        }
+
+    }
+    class Corner
+    {
+        privat int gradus;
+        privat int min;
+        privat int sec;
+
+
+
+        public Corner(int gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec= sec;
+        }
+
+        public static double ToRadians(this int gradus)
+        {
+            return (Math.PI / 180) * gradus;
+        }
+    }
+}
